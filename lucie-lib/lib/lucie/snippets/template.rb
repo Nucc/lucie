c60496570e26ccd324a430132cf8d140e388c347
Nucc <nucc@bteam.hu>
2013-04-13 18:59:04 +0100

    Refactored the Snippets::Template file.

--- lucie-lib/lib/lucie/snippets/template.rb
+++ lucie-lib/lib/lucie/snippets/template.rb
@@ -4,30 +4,63 @@ require 'fileutils'
 module Lucie
   module Snippets
     module Template
-      private
+
+    protected
+
+      # that would be great if we occurre the Helper init in every method, but
+      # now I don't know nice solution for this
+
       def create_file(file_path, &block)
-
-        file_path = [app.root, file_path].join("/") if file_path[0] != "/"
-
-        dir_name = File.dirname(file_path)
-        if !File.directory?(dir_name)
-          FileUtils.mkdir_p(dir_name)
-        end
-
-        if block
-          File.open(file_path, "a") do |f|
-            yield(f)
-          end
-        else
-          File.new(file_path, "a")
-        end
+        @file_utils_helper ||= FileUtilsHelper.new(self)
+        @file_utils_helper.create_file(file_path, &block)
       end
 
       def template(template, target)
+        @file_utils_helper ||= FileUtilsHelper.new(self)
         create_file(target) do |f|
           f.write ERB.new(File.read(template)).result(binding)
         end
       end
+
+    private
+
+      class FileUtilsHelper
+
+        def initialize(controller)
+          @controller = controller
+        end
+
+        def create_file(path, &content)
+          path = normalized path
+          create_dir_for path
+          add_content_to_file path, &content
+        end
+
+        def is_relative?(file_path)
+          file_path[0] != "/"
+        end
+
+        def normalized(path)
+          if is_relative?(path)
+            [@controller.app.root, path].join("/")
+          else
+            path
+          end
+        end
+
+        def create_dir_for(file_path)
+          dir_name = File.dirname(file_path)
+          if !File.directory?(dir_name)
+            FileUtils.mkdir_p(dir_name)
+          end
+        end
+
+        def add_content_to_file(path, &content)
+          block_given? && File.open(path, "a") { |f| yield f } || File.new(path, "a")
+        end
+
+      end
+
     end
   end
 end
