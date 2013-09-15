7ac7a53e87f56d5cff4bd233ac1a06dba17fe9ef
Nucc <nucc@bteam.hu>
2013-09-15 20:27:36 +0100

    Try a new way of versioning.
    We need version information in lucie-bin, but $root/version
    file is not exists on that level.

--- lucie-bin/app/controllers/new_controller.rb
+++ lucie-bin/app/controllers/new_controller.rb
@@ -4,6 +4,7 @@ class NewController < Controller::Base
 
   def index
     @project_name = params.shift
+    @lucie_version = Lucie::VERSION
 
     template("new/bin/bin.tt", "#{@project_name}/bin/#{@project_name}")
     template("new/app/application_controller.rb.tt", "#{@project_name}/app/controllers/application_controller.rb")
