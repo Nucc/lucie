1e201bd8db51b90e904f12cc106f5e28084c1a1a
Nucc <nucc@bteam.hu>
2013-10-18 18:05:40 +0200

    Running the app must be independent from the actual ruby env.

--- lucie-bin/app/controllers/new_controller.rb
+++ lucie-bin/app/controllers/new_controller.rb
@@ -8,9 +8,13 @@ class NewController < Controller::Base
 
     template("new/bin/bin.tt", "#{@project_name}/bin/#{@project_name}")
     template("new/app/application_controller.rb.tt", "#{@project_name}/app/controllers/application_controller.rb")
+    template("new/app/boot.rb.tt", "#{@project_name}/app/boot.rb")
     template("new/Gemfile.tt", "#{@project_name}/Gemfile")
+    template("new/Rakefile.tt", "#{@project_name}/Rakefile")
 
     File.chmod(0755, "#{app.pwd}/#{@project_name}/bin/#{@project_name}")
+
+    system "cd #{app.pwd}/#{@project_name} && rake setup"
   end
 
   def no_method
